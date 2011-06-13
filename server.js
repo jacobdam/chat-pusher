69cef2afecd89feeb6450bf65b975fa4b037bc3e
Jacob Dam <jdam@digication.com>
2011-06-13 19:00:50 +0700

    update web port

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var http = require('http');
+var http = require('http'),
+    process = require('process');
+
+var port = process.env.PORT || 4000;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, "0.0.0.0");
+}).listen(port, "0.0.0.0");
