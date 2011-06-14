909967726a15f4b429dd4549d14af40318a024ae
Jacob Dam <jdam@digication.com>
2011-06-14 15:00:46 +0700

    add server log

--- server.js
+++ server.js
@@ -2,5 +2,17 @@ var Faye   = require('faye');
 
 
 var server = new Faye.NodeAdapter({mount: '/faye'});
+var Logger = {
+    incoming: function(message, callback) {
+        console.log('[incoming]', message)
+        callback(message)
+    },
+    outgoing: function(message, callback) {
+        console.log('[outgoing]', message)
+        callback(message)
+    }
+}
+server.addExtension(Logger)
 
 server.listen(process.env.PORT || 8000);
+console.info('Server started')
